7b1c7ec1c7558160980060189a9e29b74fb28975
Jake Schwarztrauber <jakeschwa@gmail.com>
2019-08-26 22:52:07 -0400

    workig on ceiling

--- src/Ceiling.java
+++ src/Ceiling.java
@@ -1,4 +1,6 @@
 import netscape.javascript.JSObject;
+import org.json.JSONObject;
+import org.json.JSONString;
 
 import java.awt.*;
 import java.util.Random;
@@ -11,40 +13,73 @@ public class Ceiling {
     Point bottomLeft;
     Point bottomRight;
 
-    int numberOfPoints;
-    int[] surface;
+    int [ ] ceilingX;
+    int [ ] ceilingY;
 
-    int rate;
+    int numberOfPoints;
+
+
+
+
+
     int topBound;
     int bottomBound;
     int leftBound;
     int rightBound;
 
+    int roughness = 20;
+
+    int[] surfaceX = new int[roughness];
+    int[] surfaceY = new int[roughness];
+
+
+
 
     Ceiling(){
-        rightBound = 1000;
         leftBound = 0;
+        rightBound = 1000;
+        topBound = 0;
+        bottomBound = 500;
+
         topLeft = new Point(0, 0);
         topRight = new Point(1000, 0);
         bottomLeft = new Point(0,500 );
         bottomRight = new Point(1000, 500);
-        int [ ] x = {0,1000,1000,0};
-        int [ ] y = {0,0,50,50};
+
+        int [ ] ceilingX;
+        int [ ] ceilingY;
+
         int numberOfPoints = 4;
-        shape = new Polygon(x,y,numberOfPoints);
+
+
+        JSONObject test = generateSurface();
+
+        System.out.println(surfaceX.length);
+
     }
 
-    private JSObject generateSurface(){
-        int[] x;
-        int[] y;
-        int bumps = 20;
+    private JSONObject generateSurface(){
+
+
         Random random = new Random();
 
-        for(int i = leftBound; i<=rightBound; i+=bumps){
-            x[i] = random.nextInt()
+        for(int i = 0; i<surfaceX.length; i++){
+            surfaceY[i] = random.nextInt(bottomBound);
+            surfaceX[i] = i;
+
+
+
         }
 
-        return null;
+        JSONObject surfaceCords = new JSONObject();
+
+        surfaceCords.put("x",surfaceX);
+        surfaceCords.put("y",surfaceY);
+
+        System.out.println(surfaceCords.toString());
+
+
+        return surfaceCords;
     }
 
 
