1d340dfd47b5d86f785bfd2eebb69d10236c7413
Jake Schwarztrauber <jakeschwa@gmail.com>
2019-09-01 13:50:09 -0400

    enviorment moves. refactored ceiling and floor. Runs

--- src/Ceiling.java
+++ src/Ceiling.java
@@ -1,42 +1,41 @@
-import netscape.javascript.JSObject;
-import org.json.JSONObject;
-import org.json.JSONString;
-
 import java.awt.*;
 import java.util.Random;
 
 public class Ceiling {
+
+    int numOfPixelsBetweenSurfacePoints = 20;
+
+    int lengthOfCeiling = 6000;
+
+    int NumberOfPolygonPoints = lengthOfCeiling / numOfPixelsBetweenSurfacePoints + 2;
+
+    int numberOfSurfaces = lengthOfCeiling / numOfPixelsBetweenSurfacePoints;
+
+    int [ ] ceilingX = new int[NumberOfPolygonPoints];
+    int [ ] ceilingY = new int[NumberOfPolygonPoints];
+
+    int[] surfaceX = new int[numberOfSurfaces];
+    int[] surfaceY = new int[numberOfSurfaces];
+
     public Polygon shape;
-    Point topLeft;
-    Point topRight;
-    Point bottomLeft;
-    Point bottomRight;
-    int numberOfPoints;
-    int topBound;
-    int bottomBound;
-    int leftBound;
-    int rightBound;
 
-    int[] surfaceX = new int[21];
-    int[] surfaceY = new int[21];
-
-    int [ ] ceilingX = new int[23];
-    int [ ] ceilingY = new int[23];
     Ceiling(){
-        leftBound = 0;
-        rightBound = 1000;
-        topBound = 0;
-        bottomBound = 200;
-        topLeft = new Point(0, 0);
-        topRight = new Point(1000, 0);
-        bottomLeft = new Point(0,100 );
-        bottomRight = new Point(1000, 100);
-        generateSurface();
+        generateCeilingSurface();
+        generateCeilingPolygon();
+    }
 
-        for( int i = 0; i<ceilingX.length; i++){
+    private void generateCeilingSurface(){
+        Random random = new Random();
+        for(int i = 0; i<surfaceX.length; i++){
+            surfaceY[i] = random.nextInt(100) + 50;
+            surfaceX[i] = i * numOfPixelsBetweenSurfacePoints;
+        }
+    }
 
+    private void generateCeilingPolygon(){
+        for( int i = 0; i<NumberOfPolygonPoints; i++){
             if(i == 0) {
-                ceilingX[i] = 1000;
+                ceilingX[i] = 6000;
                 ceilingY[i] = 0;
             }else if( i == 1 ){
                 ceilingX[i] = 0;
@@ -46,16 +45,16 @@ public class Ceiling {
                 ceilingY[i] = surfaceY[i-2];
             }
         }
-        shape = new Polygon(ceilingX, ceilingY, 23);
-        System.out.print(shape.xpoints);
-
+        updateShape();
     }
 
-    private void generateSurface(){
-        Random random = new Random();
-        for(int i = 0; i<surfaceX.length; i++){
-            surfaceY[i] = random.nextInt(100) + 50;
-            surfaceX[i] = i * 50;
+    public void moveSurface(){
+        for(int i = 0; i<NumberOfPolygonPoints; i++ ){
+            ceilingX[i]-=1;
         }
+        updateShape();
+    }
+    private void updateShape(){
+        shape = new Polygon(ceilingX, ceilingY, NumberOfPolygonPoints);
     }
 }
--- src/Floor.java
+++ src/Floor.java
@@ -2,57 +2,60 @@ import java.awt.*;
 import java.util.Random;
 
 public class Floor {
+
+    int numOfPixelsBetweenSurfacePoints = 20;
+
+    int lengthOfCeiling = 6000;
+
+    int NumberOfPolygonPoints = lengthOfCeiling / numOfPixelsBetweenSurfacePoints + 2;
+
+    int numberOfSurfacePoints = lengthOfCeiling / numOfPixelsBetweenSurfacePoints;
+
+    int [ ] polygonX = new int[NumberOfPolygonPoints];
+    int [ ] polygonY = new int[NumberOfPolygonPoints];
+
+    int[] surfaceX = new int[numberOfSurfacePoints];
+    int[] surfaceY = new int[numberOfSurfacePoints];
+
     public Polygon shape;
-    Point topLeft;
-    Point topRight;
-    Point bottomLeft;
-    Point bottomRight;
 
-    int topBound;
-    int bottomBound;
-    int leftBound;
-    int rightBound;
-
-    int[] surfaceX = new int[21];
-    int[] surfaceY = new int[21];
-
-    int [ ] ceilingX = new int[23];
-    int [ ] ceilingY = new int[23];
     Floor(){
-        leftBound = 0;
-        rightBound = 1000;
-        topBound = 0;
-        bottomBound = 200;
-        topLeft = new Point(0, 0);
-        topRight = new Point(1000, 0);
-        bottomLeft = new Point(0,100 );
-        bottomRight = new Point(1000, 100);
-        generateSurface();
-
-        for( int i = 0; i<ceilingX.length; i++){
-
-            if(i == 0) {
-                ceilingX[i] = 1000;
-                ceilingY[i] = 500;
-            }else if( i == 1 ){
-                ceilingX[i] = 0;
-                ceilingY[i] = 500;
-            }else if(i>1){
-                ceilingX[i] = surfaceX[i-2];
-                ceilingY[i] = surfaceY[i-2];
-            }
-        }
-        shape = new Polygon(ceilingX, ceilingY, 23);
-        System.out.print(shape.xpoints);
-
+        generateCeilingSurface();
+        generateCeilingPolygon();
     }
 
-    private void generateSurface(){
+    private void generateCeilingSurface(){
         Random random = new Random();
         for(int i = 0; i<surfaceX.length; i++){
-            surfaceY[i] = random.nextInt(100) + 350;
-            surfaceX[i] = i * 50;
+            surfaceY[i] = random.nextInt(100) + 300;
+            surfaceX[i] = i * numOfPixelsBetweenSurfacePoints;
         }
+    }
+
+    private void generateCeilingPolygon(){
+        for( int i = 0; i<NumberOfPolygonPoints; i++){
+            if(i == 0) {
+                polygonX[i] = 6000;
+                polygonY[i] = 500;
+            }else if( i == 1 ){
+                polygonX[i] = 0;
+                polygonY[i] = 500;
+            }else if(i>1){
+                polygonX[i] = surfaceX[i-2];
+                polygonY[i] = surfaceY[i-2];
+            }
+        }
+        updatePolygon();
+    }
+
+    public void moveSurface(){
+        for(int i = 0; i<NumberOfPolygonPoints; i++ ){
+            polygonX[i]-=1;
+        }
+        updatePolygon();
+    }
+    private void updatePolygon(){
+        shape = new Polygon(polygonX, polygonY, NumberOfPolygonPoints);
     }
 }
 
