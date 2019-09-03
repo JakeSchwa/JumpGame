8927ad24e7baa40e25ddf16c9adf2629ff604d2f
Jake Schwarztrauber <jakeschwa@gmail.com>
2019-09-03 19:07:13 -0400

    added enemy spawn, shooting, enemy simple movement, bullets remove after they leave the screen, ect. About to refactor structure of software

--- src/Enviorment.java
+++ src/Enviorment.java
@@ -4,15 +4,32 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Random;
 
 public class Enviorment {
     Ceiling ceiling;
     Floor floor;
+    LinkedList<Enemy> enemies = new LinkedList<Enemy>();
 
 
     Enviorment(){
         ceiling = new Ceiling();
         floor = new Floor();
+        spawnEnemyWaves();
+    }
+
+    public void spawnEnemyWaves(){
+        for(int i = 0; i < 60; i+=12){
+            enemies.add(new Enemy( new Point(800, i + 200)));
+        }
+    }
+
+    public void checkForNextWave(){
+        if(enemies.size() == 0){
+            spawnEnemyWaves();
+        }
     }
 
 
--- src/Ship.java
+++ src/Ship.java
@@ -1,23 +1,63 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 
 public class Ship extends JPanel{
-    Point position;
     int velX = 3;
     int velY = 3;
 
-    Ship(){
-        position = new Point(100, 100);
+    LinkedList<Bullet> bullets = new LinkedList<Bullet>();
+
+    int height = 10;
+    int length = 10;
+
+    Polygon shape;
+
+    int x;
+    int y;
+
+    int[] xCords = new int[4];
+    int[] yCords = new int[4];
+
+    Ship(Point startPosition){
+        x = startPosition.x;
+        y = startPosition.y;
+        drawShip();
     }
 
-    //Functions move the ship
-    public void up(){ position.y -= velY; }
+    public void drawShip(){
+        xCords[0] = x;
+        xCords[1] = x + length;
+        xCords[2] = x + length;
+        xCords[3] = x;
 
-    public void down(){ position.y += velY; }
+        yCords[0] = y;
+        yCords[1] = y;
+        yCords[2] = y + height;
+        yCords[3] = y + height;
 
-    public void left(){ position.x -= velX; }
+        shape = new Polygon(xCords,yCords,4);
+    }
 
-    public void right(){ position.x += velX; }
+    public Polygon getShip(){
+        drawShip();
+        return shape;
+    }
+
+    public void shoot(){
+        bullets.add(new Bullet(new Point(x,y)));
+    }
+
+
+
+    public void up(){ y -= velY; }
+
+    public void down(){ y += velY; }
+
+    public void left(){ x -= velX; }
+
+    public void right(){ x += velX; }
 
 }
